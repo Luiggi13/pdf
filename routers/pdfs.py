8a9759e9bbe3a7c803d8286dd4f6151b4cd99496
Christian Llansola <christian.llansola@mail.schwarz>
2024-10-25 22:55:24 +0200

    upload files and compress it

--- routers/pdfs.py
+++ routers/pdfs.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import os
+import aiofiles
+from fastapi import APIRouter, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi import Request
 
@@ -9,3 +11,15 @@ router = APIRouter()
 @router.get("/pdf", tags=["PDF"], summary="Resize pdf file size", description="")
 def get_pdfs(input: str, request: Request):
     return JSONResponse(content={"message": compress(input)}, headers={"X-API": "asdsa"})
+    
+@router.post("/pdf/upload", tags=["PDF"], summary="Upload pdf file", description="")
+async def create_upload_file(file: UploadFile | None = None):
+    async with aiofiles.open("./inputs/" + file.filename, 'wb') as out_file:
+            while content := await file.read(1024):
+                await out_file.write(content)
+    compress(file.filename.split('.')[0])
+    os.remove("./inputs/" + file.filename)
+    if not file:
+        return {"message": "fileName"}
+    else:
+        return {"message": "File processed successfully", "size": file.size}
