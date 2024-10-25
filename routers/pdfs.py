9267e37c0f3252fc77cdc91670be3531bc6b871f
Christian Llansola <christian.llansola@mail.schwarz>
2024-10-25 23:23:47 +0200

    unsupported type removed

--- routers/pdfs.py
+++ routers/pdfs.py
@@ -9,7 +9,7 @@ from utils.utils import compress
 router = APIRouter()
     
 @router.post("/pdf/upload", tags=["PDF"], summary="Upload pdf file", description="")
-async def create_upload_file(file: UploadFile | None = None):
+async def create_upload_file(file: UploadFile):
     async with aiofiles.open("./inputs/" + file.filename, 'wb') as out_file:
             while content := await file.read(1024):
                 await out_file.write(content)
