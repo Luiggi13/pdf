3ba961d3de6bf6f2b16c96bc0434a932900df6e9
Christian Llansola <christian.llansola@mail.schwarz>
2024-10-26 11:58:49 +0200

    Upload fixed

--- routers/pdfs.py
+++ routers/pdfs.py
@@ -1,15 +1,13 @@
 import os
 import aiofiles
-from fastapi import APIRouter, UploadFile
-from fastapi.responses import JSONResponse
-from fastapi import Request
+from fastapi import APIRouter, File, UploadFile
 
 from utils.utils import compress
 
 router = APIRouter()
     
 @router.post("/pdf/upload", tags=["PDF"], summary="Upload pdf file", description="")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile = File(...)):
     async with aiofiles.open("./inputs/" + file.filename, 'wb') as out_file:
             while content := await file.read(1024):
                 await out_file.write(content)
